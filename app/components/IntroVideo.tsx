56be93eb6a429a90f7a7434df2f56fc88dc9834e
fardeenpathan <md.fardeen5236@gmail.com>
2026-09-08 10:45:27 +0530

    old code

--- app/components/IntroVideo.tsx
+++ app/components/IntroVideo.tsx
@@ -51,44 +51,6 @@ export default function IntroVideo() {
   if (!show) return null;
 
   return (
-    // <div
-    //   className={`fixed inset-0 z-[999999] transition-opacity duration-700 ${
-    //     hide ? "opacity-0 pointer-events-none" : "opacity-100"
-    //   }`}
-    //   style={{ width: "100vw", height: "100%" }}
-    // >
-    //   <video
-    //     ref={videoRef}
-    //     poster="/assets/hero_video.png"
-    //     playsInline
-    //     webkit-playsinline="true"
-    //     muted
-    //     preload="auto"
-    //     onEnded={handleEnd}
-    //     style={{
-    //       position: "absolute",
-    //       top: 0,
-    //       left: 0,
-    //       width: "100%",
-    //       height: "100%",
-    //       objectFit: "cover",
-    //       objectPosition: "center",
-    //     }}
-    //   >
-    //     <source src="/assets/hero_video.mp4" type="video/mp4" />
-    //   </video>
-
-    //   {!started && (
-    //     <button
-    //       onClick={playVideo}
-    //       className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
-    //       w-20 h-20 md:w-28 md:h-28 rounded-full   
-    //       text-black text-4xl hover:scale-110 transition cursor-pointer"
-    //     >
-         
-    //     </button>
-    //   )}
-    // </div>
 
     <div onClick={playVideo} className={`fixed inset-0 z-[999999] cursor-pointer transition-opacity duration-700 ${ hide ? "opacity-0 pointer-events-none" : "opacity-100" }`} style={{ width: "100vw", height: "100%", }} > <video ref={videoRef} poster="/assets/hero_video.png" playsInline webkit-playsinline="true" muted preload="auto" onEnded={handleEnd} style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", objectFit: "cover", objectPosition: "center", pointerEvents: "none", }} > <source src="/assets/hero_video.mp4" type="video/mp4" /> </video> </div>
   );
